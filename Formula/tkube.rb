b591c6f73a3872ce9d9cc9327a1a466777f8a43e
GitHub Action <action@github.com>
2025-08-18 22:15:34 +0000

    Update tkube to v3.0.0 - automated release update

--- Formula/tkube.rb
+++ Formula/tkube.rb
@@ -1,10 +1,10 @@
 class Tkube < Formula
   desc "Enhanced Teleport kubectl wrapper with auto-authentication"
   homepage "https://github.com/lidin10/tkube"
-  url "https://github.com/lidin10/tkube/releases/download/v2.1.2/tkube_v2.1.2_darwin_arm64.tar.gz"
-  sha256 "bec80c91980f3afaa196cd21e84acc295cf14b3a9be1e52058c214163e4a554b"
+  url "https://github.com/lidin10/tkube/releases/download/v3.0.0/tkube_v3.0.0_darwin_arm64.tar.gz"
+  sha256 "6e0b3b649cd558dc4e09fff88186dd9c457e77b27b5ebed09cb5adb1a511d812"
   license "MIT"
-  version "2.1.2"
+  version "3.0.0"
 
   depends_on "kubernetes-cli"
 
